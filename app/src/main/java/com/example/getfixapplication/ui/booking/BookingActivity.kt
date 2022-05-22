4faf4cdbcd5b37692792b4d205622a5ff6f89bd8
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2022-05-22 21:43:46 +0700

    membuat activity booking

---
+++ app/src/main/java/com/example/getfixapplication/ui/booking/BookingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.getfixapplication.ui.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking)
+    }
+}
