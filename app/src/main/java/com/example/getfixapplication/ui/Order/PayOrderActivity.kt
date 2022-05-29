261ff159e1b30bccc9885e0501e097cba6ff5b78
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2022-05-29 19:25:42 +0700

    Membuat activity pay order, detail order

---
+++ app/src/main/java/com/example/getfixapplication/ui/Order/PayOrderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.getfixapplication.ui.Order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PayOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_order)
+    }
+}
