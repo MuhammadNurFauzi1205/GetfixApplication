e3b620d5583e91bed7656623bb921785ae84ba0c
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2022-05-29 23:45:45 +0700

    menambah item teknisi dan activity_list_teknisi.xml

---
+++ app/src/main/java/com/example/getfixapplication/ui/booking/ListTeknisiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.getfixapplication.ui.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.getfixapplication.R
+
+class ListTeknisiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_teknisi)
+    }
+}
