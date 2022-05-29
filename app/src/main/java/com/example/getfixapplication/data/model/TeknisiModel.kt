39cd2b734e0ac0520ba07294e3ffc053017ce35a
Dendi Fazar Zaman <dendi.fazarzaman.tik19@mhsw.pnj.ac.id>
2022-05-30 03:23:01 +0700

    Menambahkan pilihan teknisi, dan library rating

---
+++ app/src/main/java/com/example/getfixapplication/data/model/TeknisiModel.kt
@@ -0,0 +1,11 @@
+package com.example.getfixapplication.data.model
+
+import android.media.Rating
+
+data class TeknisiModel (
+        val nama: String,
+        val alamat: String,
+        val id: String,
+        val foto: String,
+        val rating: Float
+)
---
+++ app/src/main/java/com/example/getfixapplication/ui/booking/PilihTeknisiActivity.kt
@@ -0,0 +1,68 @@
+package com.example.getfixapplication.ui.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import androidx.recyclerview.selection.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.getfixapplication.R
+import com.example.getfixapplication.data.model.TeknisiModel
+import com.example.getfixapplication.databinding.ActivityPilihTeknisiBinding
+
+class PilihTeknisiActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPilihTeknisiBinding
+   var idTeknisi: String =
+        ""
+
+    val data = listOf(
+        TeknisiModel(
+            "Teknisi 1",
+            "Jl. Teknik 1",
+            "1",
+            "https://i.pinimg.com/736x/47/a8/24/47a824c67db1ec78cc9f9011ba52022e--wallpaper-lucu-foto-lucu.jpg",
+            4.5f
+        ),
+        TeknisiModel(
+            "Teknisi 2",
+            "Jl. Teknik 2",
+            "2",
+            "https://i.ytimg.com/vi/kinwDmOO5ns/maxresdefault.jpg",
+            4.3f
+        ),
+        TeknisiModel(
+            "Teknisi 3",
+            "Jl. Teknik 3",
+            "3",
+            "https://4.bp.blogspot.com/_jJu6heRzjEA/TGJc5QBw86I/AAAAAAAAAZA/fzxpbgl-vYs/w1200-h630-p-k-no-nu/binatang+lucu.jpg",
+            3.8f
+        )
+    )
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPilihTeknisiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val teknisiAdapter = TeknisiAdapter()
+
+        binding.rvTeknisi.layoutManager = LinearLayoutManager(this)
+        binding.rvTeknisi.adapter = teknisiAdapter
+        teknisiAdapter.items = data
+        teknisiAdapter.setOnItemClickCallback(object : TeknisiAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: TeknisiModel) {
+                idTeknisi = data.id
+                Log.d("id teknisi", idTeknisi)
+            }
+        })
+
+        binding.btnPesanTeknisi.setOnClickListener {
+
+            Log.e("id teknisi", idTeknisi)
+        }
+
+    }
+
+}
