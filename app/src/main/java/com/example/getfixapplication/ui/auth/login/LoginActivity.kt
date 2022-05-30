99e7a815977270dbe866f4ba206f2fc9497bf8a5
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2022-05-31 02:13:13 +0700

    mengganti login dan signup ke firestore karena terhubung gcp

--- app/src/main/java/com/example/getfixapplication/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/example/getfixapplication/ui/auth/login/LoginActivity.kt
@@ -7,21 +7,21 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isNotEmpty
 import com.example.getfixapplication.databinding.ActivityLoginBinding
 import com.example.getfixapplication.ui.auth.register.SignupActivity
 import com.example.getfixapplication.ui.home.HomeActivity
-import com.example.getfixapplication.ui.home.HomeActivity_GeneratedInjector
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.material.textfield.TextInputLayout
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,7 +33,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
     val WebClient = "116303479224-qe4kiq0uj2dfitj8raejbd5bsgvqsja1.apps.googleusercontent.com"
-
+    val db = Firebase.firestore
     var EMAIL_KEY = "emailkey"
     var email_key = ""
 //    lateinit var  username : TextInputLayout
@@ -51,42 +51,78 @@ class LoginActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
 
 
-            myreference = FirebaseDatabase.getInstance().reference
-                .child("users").child(username.editText?.text.toString())
+//            myreference = FirebaseDatabase.getInstance().reference
+//                .child("users").child(username.editText?.text.toString())
+////
+////
+//            myreference.addListenerForSingleValueEvent(object : ValueEventListener {
+//                override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                    if (dataSnapshot.exists()) {
+//
+//                        //ambil data password dari firebase
+//                        val passwordFromFirebase = dataSnapshot.child("password").value.toString()
+//
+//                        //validasi password dengan firebase
+//                        if (passwordd.editText?.text.toString() == passwordFromFirebase) {
+//
+//                            //simpan username pada local
+//                            val sharedPreferences = getSharedPreferences(EMAIL_KEY, MODE_PRIVATE)
+//                            val editor = sharedPreferences.edit()
+//                            editor.putString(email_key, username.editText?.text.toString())
+//                            editor.apply()
+//                            //berpindah activity
+//                            val two = Intent(this@LoginActivity , HomeActivity::class.java)
+//                            startActivity(two)
+//                        } else {
+//                            Toast.makeText(applicationContext, "Password salah", Toast.LENGTH_SHORT)
+//                                .show()
+//                        }
+//                    } else {
+//                        Toast.makeText(applicationContext, "email tidak ada", Toast.LENGTH_SHORT)
+//                            .show()
+//                    }
+//                }
+//
+//                override fun onCancelled(databaseError: DatabaseError) {
+//                    Toast.makeText(applicationContext, "Database error", Toast.LENGTH_SHORT).show()
+//                }
+//            })
 
+            val user = db.collection("users")
 
-            myreference.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if (dataSnapshot.exists()) {
+            val data1 = hashMapOf(
+                username.editText?.text.toString() to "username",
+                passwordd.editText?.text.toString() to "password"
+            )
+            user.get()
 
-                        //ambil data password dari firebase
-                        val passwordFromFirebase = dataSnapshot.child("password").value.toString()
-
-                        //validasi password dengan firebase
-                        if (passwordd.editText?.text.toString() == passwordFromFirebase) {
-
-                            //simpan username pada local
-                            val sharedPreferences = getSharedPreferences(EMAIL_KEY, MODE_PRIVATE)
+                user.whereEqualTo("username", username.editText?.text.toString()).whereEqualTo("password",passwordd.editText?.text.toString())
+                .get()
+                    .addOnSuccessListener { documents ->
+                        for (document in documents) {
+                            Log.d(TAG, "${document.id} => ${document.data}")
+                            val sharedPreferences =
+                                getSharedPreferences(EMAIL_KEY, MODE_PRIVATE)
                             val editor = sharedPreferences.edit()
                             editor.putString(email_key, username.editText?.text.toString())
                             editor.apply()
                             //berpindah activity
-                            val two = Intent(this@LoginActivity , HomeActivity::class.java)
+                            val two = Intent(this@LoginActivity, HomeActivity::class.java)
                             startActivity(two)
-                        } else {
-                            Toast.makeText(applicationContext, "Password salah", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                    } else {
-                        Toast.makeText(applicationContext, "email tidak ada", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                }
 
-                override fun onCancelled(databaseError: DatabaseError) {
-                    Toast.makeText(applicationContext, "Database error", Toast.LENGTH_SHORT).show()
-                }
-            })
+                        }
+                    }
+                    .addOnFailureListener {
+                        Log.d(TAG, "username/password salah")
+                    }
+
+
+
+
+
+
+
+
         }
 
         val googlesignin = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -178,3 +214,4 @@ class LoginActivity : AppCompatActivity() {
 //        updateUI(currentUser)
 //    }
 }
+
--- app/src/main/java/com/example/getfixapplication/ui/auth/register/SignupActivity.kt
+++ app/src/main/java/com/example/getfixapplication/ui/auth/register/SignupActivity.kt
@@ -1,12 +1,17 @@
 package com.example.getfixapplication.ui.auth.register
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.getfixapplication.databinding.ActivitySignupBinding
 import com.example.getfixapplication.ui.auth.login.LoginActivity
 import com.example.getfixapplication.ui.home.HomeActivity
 import com.google.firebase.database.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +20,7 @@ class SignupActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
     var EMAIL_KEY = "emailkey"
     var email_key = ""
+    val db = Firebase.firestore
 
 //    lateinit var usernamee: TextInputLayout
 //    lateinit var passwordd: TextInputLayout
@@ -28,31 +34,53 @@ class SignupActivity : AppCompatActivity() {
         val emaill=binding.ed2
         val passwordd=binding.ed3
 
-
         binding.btnSign.setOnClickListener {
             //simpan username pada local
             val sharedPreferences = getSharedPreferences(EMAIL_KEY, MODE_PRIVATE)
             val editor = sharedPreferences.edit()
             editor.putString(email_key, usernamee.editText?.text.toString())
             editor.apply()
+//
+//
+//            //simpan username pada firebase
+//            myreference = FirebaseDatabase.getInstance().reference
+//                .child("users").child(usernamee.editText?.text.toString())
+//
+//            myreference.addListenerForSingleValueEvent(object : ValueEventListener {
+//                override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                    dataSnapshot.ref.child("username").setValue(usernamee.editText?.text.toString())
+//                    dataSnapshot.ref.child("email").setValue(emaill.editText?.text.toString())
+//                    dataSnapshot.ref.child("password").setValue(passwordd.editText?.text.toString())
+//                }
+//
+//                override fun onCancelled(databaseError: DatabaseError) {}
+//            })
+//            val home = Intent(this, HomeActivity::class.java)
+//            startActivity(home)
+////            Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
 
-
-            //simpan username pada firebase
-            myreference = FirebaseDatabase.getInstance().reference
-                .child("users").child(usernamee.editText?.text.toString())
-
-            myreference.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    dataSnapshot.ref.child("username").setValue(usernamee.editText?.text.toString())
-                    dataSnapshot.ref.child("email").setValue(emaill.editText?.text.toString())
-                    dataSnapshot.ref.child("password").setValue(passwordd.editText?.text.toString())
+            val username = usernamee.editText?.text.toString()
+            val email = emaill.editText?.text.toString()
+            val password = passwordd.editText?.text.toString()
+            val user = hashMapOf(
+                "username" to username,
+                "email" to email,
+                "password" to password
+            )
+            // Add a new document with a generated ID
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    val home = Intent(this, HomeActivity::class.java)
+                    startActivity(home)
+                    Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
                 }
 
-                override fun onCancelled(databaseError: DatabaseError) {}
-            })
-            val home = Intent(this, HomeActivity::class.java)
-            startActivity(home)
-//            Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+
         }
 
         binding.tvActionLogin.setOnClickListener {
