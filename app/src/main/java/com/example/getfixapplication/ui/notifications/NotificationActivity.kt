83d0570bec971589789b91de9990a44af0041821
Dendi Fazar Zaman <dendi.fazarzaman.tik19@mhsw.pnj.ac.id>
2022-06-04 18:24:40 +0700

    Menambahkan action back button di toolbar

--- app/src/main/java/com/example/getfixapplication/ui/notifications/NotificationActivity.kt
+++ app/src/main/java/com/example/getfixapplication/ui/notifications/NotificationActivity.kt
@@ -3,10 +3,19 @@ package com.example.getfixapplication.ui.notifications
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.getfixapplication.R
+import com.example.getfixapplication.databinding.ActivityNotificationBinding
 
 class NotificationActivity : AppCompatActivity() {
+
+    private lateinit var notificationBinding: ActivityNotificationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
+        notificationBinding = ActivityNotificationBinding.inflate(layoutInflater)
+        setContentView(notificationBinding.root)
+
+        notificationBinding.tbNotificationToolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
