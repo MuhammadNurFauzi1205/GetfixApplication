1c0b754b0a97730079b942ce462defc2a8518f8a
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2022-06-06 23:02:24 +0700

    menambah model tflite, dan logicnya

--- app/src/main/java/com/example/getfixapplication/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/example/getfixapplication/ui/camera/CameraActivity.kt
@@ -1,10 +1,16 @@
 package com.example.getfixapplication.ui.camera
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
@@ -16,10 +22,14 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.getfixapplication.R
 import com.example.getfixapplication.databinding.ActivityCameraBinding
+import com.example.getfixapplication.ml.Model
 import com.example.getfixapplication.utils.ConstVal.CAMERA_X_RESULT
 import com.example.getfixapplication.utils.ConstVal.PERMISSIONS
 import com.example.getfixapplication.utils.createFile
 import com.example.getfixapplication.utils.showToast
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -29,6 +39,7 @@ class CameraActivity : AppCompatActivity() {
     private var imageCapture: ImageCapture? = null
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
     private lateinit var cameraExecutor: ExecutorService
+    lateinit var bitmap: Bitmap
 
     private val permReqLauncher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
@@ -68,6 +79,48 @@ class CameraActivity : AppCompatActivity() {
         }
 
         binding.ivCameraCapture.setOnClickListener { takePhoto() }
+
+        val labels = application.assets.open("label.txt").bufferedReader().use { it.readText() }.split("\n")
+
+//        select_image_button.setOnClickListener(View.OnClickListener {
+//            Log.d("mssg", "button pressed")
+//            var intent : Intent = Intent(Intent.ACTION_GET_CONTENT)
+//            intent.type = "image/*"
+//
+//            startActivityForResult(intent, 250)
+//        })
+
+
+//            var resized = Bitmap.createScaledBitmap(bitmap, 224, 224, true)
+            val model = Model.newInstance(this)
+
+//            var tbuffer = TensorImage.fromBitmap(resized)
+//            var byteBuffer = tbuffer.buffer
+//
+//// Creates inputs for reference.
+//            val inputFeature0 =
+//                TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+//            inputFeature0.loadBuffer(byteBuffer)
+
+            val inputFeature0 =
+                TensorBuffer.createFixedSize(intArrayOf(1, 150, 150, 3), DataType.FLOAT32)
+
+            val input = Bitmap.createScaledBitmap(bitmap ,150, 150, true)
+            val image = TensorImage(DataType.FLOAT32)
+            image.load(input)
+            val byteBuffer = image.buffer
+            inputFeature0.loadBuffer(byteBuffer)
+
+// Runs model inference and gets result.
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+            var max = getMax(outputFeature0.floatArray)
+
+            binding.tvCameraUpload.setText(labels[max])
+
+// Releases model resources if no longer used.
+            model.close()
     }
 
     public override fun onResume() {
@@ -138,4 +191,35 @@ class CameraActivity : AppCompatActivity() {
             }
         }, ContextCompat.getMainExecutor(this))
     }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+
+            if(requestCode == 250){
+                binding.ivCameraCapture.setImageURI(data?.data)
+
+                var uri : Uri?= data?.data
+                bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+            }
+            else if(requestCode == 200 && resultCode == Activity.RESULT_OK){
+                bitmap = data?.extras?.get("data") as Bitmap
+                binding.ivCameraCapture.setImageBitmap(bitmap)
+            }
+
+        }
+
+        fun getMax(arr:FloatArray) : Int{
+            var ind = 0;
+            var min = 0.0f;
+
+            for(i in 0..1)
+            {
+                if(arr[i] > min)
+                {
+                    min = arr[i]
+                    ind = i;
+                }
+            }
+            return ind
+        }
 }
